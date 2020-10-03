7dbb4d973a354dbe61a45015ca9f664c862e8aac
Alexander Kaymanov <alserg1059@yandex.ru>
2020-10-03 14:27:53 +0400

    Добавлена интерактивность profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,30 @@
-const buttonOpenPopup = document.querySelector(".profile__edit-button");
-const buttonClosePopup = document.querySelector(".close-image");
-const popup = document.querySelector(".overlay");
+const buttonOpenPopup = document.querySelector('.profile__edit-button');
+const buttonClosePopup = document.querySelector('.close-image');
+const buttonSubmitPopup = document.querySelector('.form-edit__submit-button');
+const popup = document.querySelector('.overlay');
 
-const popupToggle = () => {
-  popup.classList.toggle("overlay_is-opened");
+let formElement = document.querySelector('.form-edit');
+let profileName = document.querySelector('.profile__name');
+let profileAboutYourself = document.querySelector('.profile__about-yourself');
+let nameInput = document.querySelector('.form-edit__input-name');
+let jobInput = document.querySelector('.form-edit__input-about-yourself');
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileAboutYourself.textContent;
+
+// Обработчик «отправки» формы
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileName.textContent = nameInput.value;
+  profileAboutYourself.textContent = jobInput.value;
 }
 
-buttonOpenPopup.addEventListener("click", popupToggle);
-buttonClosePopup.addEventListener("click", popupToggle);
+// Обработчик открытия и закрытия модального окна (попапа).
+const popupToggle = () => {
+  popup.classList.toggle('overlay_is-opened');
+}
+
+buttonOpenPopup.addEventListener('click', popupToggle);
+buttonClosePopup.addEventListener('click', popupToggle);
+buttonSubmitPopup.addEventListener('click', popupToggle);
+formElement.addEventListener('submit', formSubmitHandler);
