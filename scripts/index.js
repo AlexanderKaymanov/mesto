7aa1af386d1c997c1972413cc46d37175fcf1e1a
Alexander Kaymanov <alserg1059@yandex.ru>
2020-10-05 13:09:30 +0400

    Добавлены исправления по ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
 const buttonOpenPopup = document.querySelector('.profile__edit-button');
-const buttonClosePopup = document.querySelector('.close-image');
-const buttonSubmitPopup = document.querySelector('.form-edit__submit-button');
+const buttonClosePopup = document.querySelector('.form-edit__close-button');
 const popup = document.querySelector('.overlay');
 
 let formElement = document.querySelector('.form-edit');
 let profileName = document.querySelector('.profile__name');
 let profileAboutYourself = document.querySelector('.profile__about-yourself');
-let nameInput = document.querySelector('.form-edit__input-name');
-let jobInput = document.querySelector('.form-edit__input-about-yourself');
+let nameInput = document.querySelector('.form-edit__input_name');
+let jobInput = document.querySelector('.form-edit__input_about-yourself');
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileAboutYourself.textContent;
+// Обработчик переноса полей из профиля пользователя в попап
+function textTransmittingFields () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAboutYourself.textContent;
+}
 
 // Обработчик «отправки» формы
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   profileName.textContent = nameInput.value;
   profileAboutYourself.textContent = jobInput.value;
+  popupToggle();
 }
 
 // Обработчик открытия и закрытия модального окна (попапа).
 const popupToggle = () => {
   popup.classList.toggle('overlay_is-opened');
-}
+  textTransmittingFields ();
+};
 
+formElement.addEventListener('submit', formSubmitHandler);
 buttonOpenPopup.addEventListener('click', popupToggle);
 buttonClosePopup.addEventListener('click', popupToggle);
-buttonSubmitPopup.addEventListener('click', popupToggle);
-formElement.addEventListener('submit', formSubmitHandler);
