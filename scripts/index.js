ec3f5c901fe530c9c1d8931cd2f89a46ac274b2c
Alexander Kaymanov <alserg1059@yandex.ru>
2020-10-07 10:48:13 +0400

    Добавлены изменения по ревью 3

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,11 @@ let jobInput = document.querySelector('.form-edit__input_about-yourself');
 // Обработчик открытия и закрытия модального окна (попапа).
 const popupToggle = () => {
   popup.classList.toggle('overlay_is-opened');
-  textTransmittingFields ();
-};
-
-// Обработчик переноса полей из профиля пользователя в попап
-function textTransmittingFields () {
   if (popup.classList.contains('overlay_is-opened') === true) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileAboutYourself.textContent;
   }
-}
+};
 
 // Обработчик «отправки» формы
 function formSubmitHandler (evt) {
@@ -30,7 +25,6 @@ function formSubmitHandler (evt) {
   popupToggle();
 }
 
-textTransmittingFields ();
 formElement.addEventListener('submit', formSubmitHandler);
 buttonOpenPopup.addEventListener('click', popupToggle);
 buttonClosePopup.addEventListener('click', popupToggle);
