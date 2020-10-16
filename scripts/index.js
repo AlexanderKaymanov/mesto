0979189e1dfea8d5eb4ea0c3a7935897c9065238
Alexander Kaymanov <alserg1059@yandex.ru>
2020-10-16 16:55:20 +0400

    Добавлены кнопки remove и like

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,65 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 buttonOpenPopup.addEventListener('click', popupToggle);
 buttonClosePopup.addEventListener('click', popupToggle);
+
+// Инициализация 6-ти карточек mesto по умолчанию
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cards = document.querySelector('.cards');
+const template = document.querySelector('.template');
+
+const renderCard = () => {
+  const items = initialCards.map(element => getItems(element));
+  cards.append(...items);
+};
+
+const handlerRemove = (evt) => {
+  evt.target.closest('.card').remove();
+};
+
+const handlerLike = (evt) => {
+  evt.target.classList.toggle('card__button_like-active');
+};
+
+const getItems = (data) => {
+  const card = template.content.cloneNode(true);
+  card.querySelector('.card__text').textContent = data.name;
+  card.querySelector('.card__image').src = data.link;
+
+  const removeButton = card.querySelector('.card__button_remove');
+  const likeButton = card.querySelector('.card__button_like');
+
+  removeButton.addEventListener('click', handlerRemove);
+  likeButton.addEventListener('click', handlerLike);
+
+  return card;
+};
+
+renderCard();
+
+// console.log (cards);
